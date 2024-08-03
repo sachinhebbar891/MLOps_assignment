ed58bc1f7b49a1d2f1baf0501533c50d7ede78fa
sachinhebbar891 <79380274+sachinhebbar891@users.noreply.github.com>
2024-08-03 18:28:14 +0530

    Update data path

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,7 +6,7 @@ def test_prediction():
     scaler = joblib.load('model/standard_scaler.joblib')
     model = joblib.load('model/lr_model.joblib')
 
-    new_data = pd.read_csv('../data/test.csv')
+    new_data = pd.read_csv('data/test.csv')
     X_test = new_data.drop(['Outcome'], axis=1)
     X_test_scaled = scaler.transform(X_test)
     predictions = model.predict(X_test_scaled)
