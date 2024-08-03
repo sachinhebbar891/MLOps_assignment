5c4a9d4723edad76ea1bc13afc9925aa8ad04ccb
sachinhebbar891 <79380274+sachinhebbar891@users.noreply.github.com>
2024-08-03 17:59:21 +0530

    Update test_model.py
    Corrected the code

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def test_prediction():
     scaler = joblib.load('../model/standard_scaler.joblib')
-    model = joblib.load('../model/logistic_regression_model.joblib')
+    model = joblib.load('../model/lr_model.joblib')
     
     new_data = pd.read_csv('../data/test.csv')
     X_test = new_data.drop(['Outcome'], axis = 1)
@@ -11,7 +11,7 @@ def test_prediction():
     X_test_scaled = scaler.transform(X_test)
     predictions = model.predict(X_test_scaled)
     
-    assert len(predictions) == 2
+    assert len(predictions) == len(X_test)
 
 if __name__ == "__main__":
     test_prediction()
