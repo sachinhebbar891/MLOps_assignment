0f3b73f4603d3a8f860e208af49d24259ce7bde2
sachinhebbar891 <79380274+sachinhebbar891@users.noreply.github.com>
2024-08-03 18:11:40 +0530

    Update train.py
    Removed some spaces

--- src/train.py
+++ src/train.py
@@ -10,15 +10,15 @@ def train_model():
     data = pd.read_csv('../data/train.csv')
     X = data.drop('Outcome', axis=1)
     y = data['Outcome'].copy()
-    
+
     # Initialize and fit scaler
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X)
-    
+
     # Initialize and fit model
     model = LogisticRegression()
     model.fit(X_train_scaled, y)
-    
+
     # Save scaler and model
     joblib.dump(scaler, '../model/standard_scaler.joblib')
     joblib.dump(model, '../model/lr_model.joblib')
