46413c0eb8234881af194057ad3e94c77e2b2b9d
sachinhebbar891 <79380274+sachinhebbar891@users.noreply.github.com>
2024-08-03 18:14:39 +0530

    Update test_model.py
    removed y_test

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,13 +5,12 @@ import pandas as pd
 def test_prediction():
     scaler = joblib.load('../model/standard_scaler.joblib')
     model = joblib.load('../model/lr_model.joblib')
-    
+
     new_data = pd.read_csv('../data/test.csv')
-    X_test = new_data.drop(['Outcome'], axis = 1)
-    y_test = new_data['Outcome'].copy()
+    X_test = new_data.drop(['Outcome'],axis=1)
     X_test_scaled = scaler.transform(X_test)
     predictions = model.predict(X_test_scaled)
-    
+
     assert len(predictions) == len(X_test)
 
 
