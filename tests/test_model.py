c68bd1474feefc514f27d308c8cfc88bde077f2e
sachinhebbar891 <79380274+sachinhebbar891@users.noreply.github.com>
2024-08-03 17:54:36 +0530

    Update test_model.py
    Updated path

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,8 +2,8 @@ import joblib
 import pandas as pd
 
 def test_prediction():
-    scaler = joblib.load('model/standard_scaler.joblib')
-    model = joblib.load('model/logistic_regression_model.joblib')
+    scaler = joblib.load('../model/standard_scaler.joblib')
+    model = joblib.load('../model/logistic_regression_model.joblib')
     
     new_data = pd.read_csv('../data/test.csv')
     X_test = new_data.drop(['Outcome'], axis = 1)
