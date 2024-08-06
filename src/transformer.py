c8dbb5d55737f2171d5583ba49e695f2647276ff
sachinhebbar891 <79380274+sachinhebbar891@users.noreply.github.com>
2024-08-06 11:27:07 +0530

    Update transformer.py

--- src/transformer.py
+++ src/transformer.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def modify_data():
     data = pd.read_csv('combined_train_data.csv')
-    data.drop(data.tail(5).index, inplace = True)
+    data.drop(data.tail(5).index, inplace=True)
     data.to_csv('combined_train_data.csv', index=False)
 
 
